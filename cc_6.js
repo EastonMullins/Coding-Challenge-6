d58c5a118334dde86924973da187551b7cf00453
Easton <eastonmullins@usf.edu>
2025-02-09 12:23:21 -0500

    Task 3 -Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -15,3 +15,22 @@ function calculateSalesTax(amount, taxRate){
 }
 calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
 calculateSalesTax(500, 0.1); // Expected output: "Sales Tax: $50"
+
+//Task 3 - Employee Bonus Calculation
+let calculateBonus = (salary, performanceRating) => {
+    let bonusPercentage = 0;
+
+    if(performanceRating === "Excellent"){
+        bonusPercentage = 0.20;
+    }
+    else if (performanceRating === "Good"){
+        bonusPercentage = 0.10;
+    }
+    else if (performanceRating === "Average"){
+        bonusPercentage = 0.05;
+    }
+    let bonus = salary * bonusPercentage
+    console.log(`Bonus: $${bonus}`);
+}
+calculateBonus(5000, "Excellent"); // Expected output: "Bonus: $1000"
+calculateBonus(7000, "Good");  // Expected output: "Bonus: $700"
