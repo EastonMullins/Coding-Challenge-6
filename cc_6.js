cb8684b95e4643c891143a51da327ce7f93cf488
Easton <eastonmullins@usf.edu>
2025-02-09 12:11:13 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,12 @@ function calculateProfit(costPrice, sellingPrice, unitsSold){
 }
 calculateProfit(20, 30, 100); //Expected output "Total Profit: $1000"
 calculateProfit(50, 70, 200); //Expected output: "Total Profit: $4000"
+
+//Task 2 - Sales Tax Computation
+
+function calculateSalesTax(amount, taxRate){
+    let salesTax = [amount * taxRate];
+    console.log(`Sales Tax: $${salesTax}`);
+}
+calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
+calculateSalesTax(500, 0.1); // Expected output: "Sales Tax: $50"
